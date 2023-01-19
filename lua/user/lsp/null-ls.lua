a2130aa05e3021af9cc79d7f55d132d3d017ded6
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2023-01-19 17:50:29 +0000

    fix prettierrc not being used

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -13,9 +13,7 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({
-			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-		}),
+		formatting.prettier,
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
 		formatting.rustfmt,
